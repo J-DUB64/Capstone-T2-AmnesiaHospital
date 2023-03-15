213a8cd4101b4493e19d1d3805e9cd7775c5b24c
James-Anderson <James.Anderson@tlgcohort.com>
2023-03-15 15:41:24 -0400

    Added the object of the game and presented to the user, AB#259

--- src/main/java/AmnesiaHospitalGame.java
+++ src/main/java/AmnesiaHospitalGame.java
@@ -1,5 +1,7 @@
 import java.util.Scanner;
+
 public class AmnesiaHospitalGame {
+
   public static void main(String[] args) {
     // Clear screen
     System.out.print("\033[H\033[2J");
@@ -10,5 +12,13 @@ public class AmnesiaHospitalGame {
 
     // Create scanner object for user input
     Scanner scanner = new Scanner(System.in);
+
+    // Print game information
+    System.out.println(
+        "You find yourself in a dark and abandoned hospital, with broken glass, overturned furniture, and bloodstains everywhere."
+            + " As you try to piece together what happened, you realize that you have amnesia and cannot remember anything about yourself or why you are here.");
+    System.out.println("To make matters worse, you hear the sound of shuffling feet and low growls coming from down the hall."
+        + " You realize that the hospital is infested with zombies, and you must navigate the hallways and rooms to survive and escape to the roof."
+        + " Your actions will determine the outcome of the game, so choose wisely.");
   }
 }
