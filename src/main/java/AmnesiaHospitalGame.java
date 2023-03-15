2df80c045e6706f309d9a37653fc527fb4eea6e4
James-Anderson <James.Anderson@tlgcohort.com>
2023-03-15 17:01:14 -0400

    Added user quit command, AB#261

--- src/main/java/AmnesiaHospitalGame.java
+++ src/main/java/AmnesiaHospitalGame.java
@@ -2,32 +2,48 @@ import java.util.Scanner;
 
 public class AmnesiaHospitalGame {
 
-  public static void main(String[] args) {
-    // Create scanner object for user input
-    Scanner scanner = new Scanner(System.in);
+  private static Scanner scanner = new Scanner(System.in);
 
+  public static void main(String[] args) {
     // Print game title
     System.out.println("=== Amnesia Hospital ===");
     System.out.println();
     System.out.println(
-        "Welcome to \"Amnesia Hospital,\" a horror text adventure game where you wake up in a hospital bed with no memory of who you are or how you got there."
-            + " You soon realize that the hospital is infested with zombies, and you must navigate the hallways and rooms to survive and escape."
-            + " As you explore the hospital, you will come across other survivors, some of whom may be helpful, while others may be hostile."
-            + " You will need to make choices that will determine your fate and the outcome of the game.");
+        "Welcome to \"Amnesia Hospital,\" a horror text adventure game where you wake up in a hospital bed with no memory of who you are or how you got there.");
+    System.out.println("You soon realize that the hospital is infested with zombies, and you must navigate the hallways and rooms to survive and escape.");
+    System.out.println("As you explore the hospital, you will come across other survivors, some of whom may be helpful, while others may be hostile.");
+    System.out.println("You will need to make choices that will determine your fate and the outcome of the game.");
     System.out.println();
-    // Wait for a key to be pressed
-    System.out.println("Press any key to continue...");
-    scanner.nextLine();
+
+    // Start the game
+    startGame();
+  }
+
+  public static String getUserInput() {
+    String input = scanner.nextLine();
+    if (input.equalsIgnoreCase("quit")) {
+      System.out.println("Are you sure you want to quit? (y/n)");
+      input = scanner.nextLine();
+      if (input.equalsIgnoreCase("y")) {
+        System.out.println("Thanks for playing Amnesia Hospital. Goodbye!");
+        System.exit(0);
+      }
+    }
+    return input;
+  }
+
+  public static void startGame() {
+    System.out.println("Press Enter to continue...");
+    getUserInput();
 
     // Clear screen
     System.out.print("\033[H\033[2J");
     System.out.flush();
 
-
     while (true) {
       // Prompt the player to start a new game
       System.out.println("Do you want to start a new game? (y/n)");
-      String input = scanner.nextLine();
+      String input = getUserInput();
       if (input.equalsIgnoreCase("y")) {
         // Clear screen
         System.out.print("\033[H\033[2J");
@@ -41,12 +57,13 @@ public class AmnesiaHospitalGame {
             "To make matters worse, you hear the sound of shuffling feet and low growls coming from down the hall."
                 + " You realize that the hospital is infested with zombies, and you must navigate the hallways and rooms to survive and escape to the roof."
                 + " Your actions will determine the outcome of the game, so choose wisely.");
+
+        // Add game logic here
+
       } else {
-        // Exit the game
         System.out.println("Thanks for playing Amnesia Hospital. Goodbye!");
         break;
       }
     }
-
   }
 }
