4f8434e57dd29ea51e9f254da52c1e077d8163d0
jmbarnes114 <jon.barnes@tlgcohort.com>
2023-03-21 14:48:50 -0700

    Created Command.java, CommandLoader, command_data.json. The player will be able to make commands such as drop item, look around, and help

---
+++ src/main/java/Command.java
@@ -0,0 +1,22 @@
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public class Command {
+  private String name;
+  private String description;
+
+  @JsonCreator
+  public Command(@JsonProperty("name") String name, @JsonProperty("description") String description) {
+    this.name = name;
+    this.description = description;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public String getDescription() {
+    return description;
+  }
+}
+
--- src/main/java/com/tlglearning/amnesiahospital/model/JsonData.java
+++ src/main/java/com/tlglearning/amnesiahospital/model/JsonData.java
@@ -71,4 +71,6 @@ public class JsonData {
     }
     return gameData;
   }
+
+
 }
