27ac6b7abfc0f253acadc70b399aa6b989a6ad78
jmbarnes114 <jon.barnes@tlgcohort.com>
2023-03-24 10:41:16 -0700

    fixed the music issue where the player can now hear background music throughout the game and will end upon quitting. AB#295

--- src/main/java/com/tlglearning/amnesiahospital/model/MusicPlayer.java
+++ src/main/java/com/tlglearning/amnesiahospital/model/MusicPlayer.java
@@ -1,25 +1,28 @@
 package com.tlglearning.amnesiahospital.model;
+
+import java.io.BufferedInputStream;
 import javax.sound.sampled.*;
-import java.io.File;
 import java.io.IOException;
-import java.net.URI;
-import java.net.URISyntaxException;
+import java.io.InputStream;
 import java.net.URL;
+import java.net.URLConnection;
 
 public class MusicPlayer {
   private Clip clip;
 
-  public void play(URL filePath) {
+  public void play(URL fileURL) {
     try {
-      File musicFile = new File(filePath.toURI());
-      AudioInputStream audioInputStream = AudioSystem.getAudioInputStream(musicFile);
+      URLConnection connection = fileURL.openConnection();
+      InputStream inputStream = connection.getInputStream();
+      BufferedInputStream bufferedInputStream = new BufferedInputStream(inputStream);
+      AudioInputStream audioInputStream = AudioSystem.getAudioInputStream(bufferedInputStream);
       AudioFormat format = audioInputStream.getFormat();
       DataLine.Info info = new DataLine.Info(Clip.class, format);
       clip = (Clip) AudioSystem.getLine(info);
       clip.open(audioInputStream);
       clip.loop(Clip.LOOP_CONTINUOUSLY);
       clip.start();
-    } catch (UnsupportedAudioFileException | IOException | LineUnavailableException | URISyntaxException e) {
+    } catch (UnsupportedAudioFileException | IOException | LineUnavailableException e) {
       e.printStackTrace();
     }
   }
